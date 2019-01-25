675e57d93b3a5854b1a5f3a780dd7299a2f19414
Felipe Claro <felipeclaro@mail.com>
2019-01-25 15:02:50 -0200

    First commit

---
+++ ProjetoModeloDDD.MVC/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProjetoModeloDDD.Domain.Entities;
+using ProjetoModeloDDD.MVC.ViewModels;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DomainToViewModelMappings"; }
+        }
+
+        protected override void Configure()
+        {
+            Mapper.CreateMap<Cliente, ClienteViewModel> ();
+            Mapper.CreateMap<Produto, ProdutoViewModel>();
+        }
+    }
+}
